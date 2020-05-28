cf4f992d2daf4ec1fc97590fdf36930e24720bab
Silas Almeida <silasadssn@gmail.com>
2020-05-28 04:06:15 -0300

    add: save to database

--- backend/src/app/Controllers/SendMessageController.js
+++ backend/src/app/Controllers/SendMessageController.js
@@ -1,13 +1,31 @@
 import Queue from '../lib/Queue';
 
+import connection from '../../config/database';
+import UserController from '../Controllers/UserController';
+
 export default {
-	send(request, response) {
-		const { users, message } = request.body;
+	async send(request, response) {
+		const { message } = request.body;
+
+		const users = await UserController.index();
 
 		Object.values(users).map(async (user) => {
 			await Queue.add('SendNewsletter', { user, message });
 		});
 
 		return response.status(200).send();
+	},
+
+	async save(user, message) {
+		const { subject, content } = message;
+		const { email } = user;
+
+		const user_id = await UserController.findId(email);
+
+		await connection('messages').insert({
+			user_id,
+			subject,
+			content,
+		});
 	},
 };
