f002b1da664d2118bde5733683878b702bd13a57
Silas Almeida <silasadssn@gmail.com>
2020-05-28 04:20:05 -0300

    add: new route: create a new client

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,11 +3,19 @@ import express from 'express';
 const routes = express.Router();
 
 import SendMessageController from './app/Controllers/SendMessageController';
+import UserController from './app/Controllers/UserController';
 
 routes.get('/', (require, response) =>
-	response.send({ greeting: 'Send Messages!' })
+	response.send({
+		greeting: 'Send Messages!',
+		routes: {
+			message: 'POST /message for send a message',
+			user: 'POST /user for create a user',
+		},
+	})
 );
 
-routes.post('/sendMessage', SendMessageController.send);
+routes.post('/message', SendMessageController.send);
+routes.post('/user', UserController.store);
 
 export default routes;
